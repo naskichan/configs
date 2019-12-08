191b905adb5463ea20a2d5b58282cc0d8ca3c782
Jonas Marowski <gitlab@naski.org>
2019-12-08 23:30:04 +0100

    Removed unnessesary cache

--- .hyper_plugins/cache/v1/npm-hyperterm-register-shortcut-1.3.0-78046af4020224a52d7e336afd4287c71770d409/modules/getShortcutFromConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = function getShortcutFromConfig (key, config) {
-  return (
-    (config.hotkeys && config.hotkeys[key]) ||
-    (config[key] && config[key]['hotkey']) ||
-    config[`${key}Shortcut`]
-  );
-}
--- .hyper_plugins/cache/v1/npm-hypertheme-0.1.0-6bbf5e2bb66f70e76179b96d195a8c63aed9aee4/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-## [0.1.0] - 2016-07-15
-
-### Added
-- Initial release
