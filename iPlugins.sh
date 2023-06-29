e93b6fd6d3cf4a95e4deeb2786a50fa8723be3ff
Naski (Jonas) <jonas.marowski@ticket.io>
2023-06-29 15:01:54 +0200

    automatic zshrc generation

--- iPlugins.sh
+++ iPlugins.sh
@@ -29,9 +29,37 @@ cd $dir
 printf "${grn}Everything looking good, well done.${end}\nNow installing plugins\n"
 git clone https://github.com/ohmyzsh/ohmyzsh.git oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git oh-my-zsh/plugins/zsh-autosuggestions
 
 printf "${grn}Repos cloned successfully!\n${end}"
-cp $configsDir/konno-yuuki.zsh-theme $dir/oh-my-zsh/themes/
-printf "${grn}Default Theme successfully installed\n${end}"
+printf "Do you want to install and enable the konno-yuuki theme? (Type y to accept)\n"
+read answer
+if [[ $answer == "y" ]]; then
+	cp $configsDir/konno-yuuki.zsh-theme $dir/oh-my-zsh/themes/
+	printf "${grn}Konno Yuuki Theme successfully installed\n${end}"
+	rccontent="ZSH_THEME=\"konno-yuuki\"\nplugins=(\n\tgit\n\tzsh-autosuggestions\n)\nsource $dir/oh-my-zsh/oh-my-zsh.sh\nsource $dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\n"
 
-printf "${grn}Done!!${end} Next steps:\n1.Copy the .zshrc according to your OS to ~/ (your home directory).\n2.Launch Zsh (type zsh, press ENTER)\nMaybe consider using Hyper if you use bare linux or WSL. Ask Jonas for help with that\n${blu}Thanks for installing using my install script!!${end}"
+else
+	printf "Skipping theme\n"
+	rccontent="ZSH_THEME=\"gentoo\"\nplugins=(git zsh-autosuggestions)\nsource $dir/oh-my-zsh/oh-my-zsh.sh\nsource $dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\n"
+fi
+
+# checks if a .zshrc file already exists in the home directory
+if [[ -e ~/.zshrc ]]; then
+	printf "${blu}A .zshrc file already exists in your home directory. Do you want to overwrite it? (Type y to accept)${end}\n"
+	read answer
+	if [[ $answer == "y" ]]; then
+		printf $rccontent > $HOME/.zshrc
+		printf "${grn}Successfully created .zshrc in home directory\n${end}"
+	else
+		printf "Skipping .zshrc\n"
+		printf "It might be necessary to append the following in your configuration: \n"
+		printf "$rccontent"
+	fi
+else
+	printf "$rccontent" > $HOME/.zshrc
+	printf "${grn}Successfully created .zshrc in home directory\n${end}"
+fi
+
+
+printf "${grn}Done!!${end}\nThanks for installing using my install script!!"
